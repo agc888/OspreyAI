8eda3da24e178ccc41a6f3500ea2258a073e0539
agc888 <agcauser1997@gmail.com>
2025-05-14 22:47:42 +1000

    Updates

---
+++ precache-manifest.f0d23013782a241bd12ca827d763a2d5.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0300dbb1b8029276c13e20373b7d79a",
+    "url": "/OspreyAI/index.html"
+  },
+  {
+    "revision": "3e0cb1e6da591094227c",
+    "url": "/OspreyAI/static/css/3.35bdca5f.chunk.css"
+  },
+  {
+    "revision": "2cea7283595869f55466",
+    "url": "/OspreyAI/static/css/4.c49d51cd.chunk.css"
+  },
+  {
+    "revision": "a0aaa02e4fd3f5c79783",
+    "url": "/OspreyAI/static/css/main.f785d234.chunk.css"
+  },
+  {
+    "revision": "23ece3d2fefa7866baee",
+    "url": "/OspreyAI/static/js/2.4465542a.chunk.js"
+  },
+  {
+    "revision": "a6c0177ea3999624a9f2d92abf620885",
+    "url": "/OspreyAI/static/js/2.4465542a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0cb1e6da591094227c",
+    "url": "/OspreyAI/static/js/3.4d664965.chunk.js"
+  },
+  {
+    "revision": "2cea7283595869f55466",
+    "url": "/OspreyAI/static/js/4.793a7b6e.chunk.js"
+  },
+  {
+    "revision": "a0aaa02e4fd3f5c79783",
+    "url": "/OspreyAI/static/js/main.1072232d.chunk.js"
+  },
+  {
+    "revision": "976a9490a47b1e4d021c",
+    "url": "/OspreyAI/static/js/runtime-main.4f890310.js"
+  },
+  {
+    "revision": "7ef1529c20ee4bcd0829040fd5851aa3",
+    "url": "/OspreyAI/static/media/10674804.7ef1529c.png"
+  },
+  {
+    "revision": "860b3c09ac348eeb37e26fcc18a6b78a",
+    "url": "/OspreyAI/static/media/10x_logo.860b3c09.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/OspreyAI/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "5c213fe16559a06dcd93d40229298306",
+    "url": "/OspreyAI/static/media/Akoya.5c213fe1.png"
+  },
+  {
+    "revision": "36cec734574fa506591b62bfd09b2770",
+    "url": "/OspreyAI/static/media/Akoya_dark.36cec734.png"
+  },
+  {
+    "revision": "370d616cf32fdc327f40109e88d63170",
+    "url": "/OspreyAI/static/media/Codex.370d616c.png"
+  },
+  {
+    "revision": "274d9072b0cece1e6fb0a004d7b4fb5b",
+    "url": "/OspreyAI/static/media/Combined.274d9072.png"
+  },
+  {
+    "revision": "104e7a48be82380f07bdc742f8568b2c",
+    "url": "/OspreyAI/static/media/GML_dark.104e7a48.png"
+  },
+  {
+    "revision": "7349a8346715082b26b8f339ac893ae8",
+    "url": "/OspreyAI/static/media/GML_light.7349a834.png"
+  },
+  {
+    "revision": "89bf3609c6e150e61ace9dbe1fd2b921",
+    "url": "/OspreyAI/static/media/Illumina_dark.89bf3609.png"
+  },
+  {
+    "revision": "030a3871520c3a821a76d55dd390588f",
+    "url": "/OspreyAI/static/media/Illumina_light.030a3871.png"
+  },
+  {
+    "revision": "4c0147815f9e9861a895984556d9ce66",
+    "url": "/OspreyAI/static/media/MK.4c014781.png"
+  },
+  {
+    "revision": "946a10e609460e9a2880124db0f87297",
+    "url": "/OspreyAI/static/media/MK.946a10e6.png"
+  },
+  {
+    "revision": "91c95ce4f19b6f7cf3e6e30f6fa06474",
+    "url": "/OspreyAI/static/media/MK_dark.91c95ce4.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/OspreyAI/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a196884ac09e84243d7b927ad44e4cf0",
+    "url": "/OspreyAI/static/media/OspreyAI_Dark.a196884a.png"
+  },
+  {
+    "revision": "9e63af0e89efe0c252dcd0802a2d2dad",
+    "url": "/OspreyAI/static/media/OspreyAI_Light.9e63af0e.png"
+  },
+  {
+    "revision": "b63fceffffb5ef164de67a13e7382036",
+    "url": "/OspreyAI/static/media/Pfizer.b63fceff.png"
+  },
+  {
+    "revision": "5f1fc6b47da9c9118b9f590710adb0ee",
+    "url": "/OspreyAI/static/media/Poster.5f1fc6b4.pdf"
+  },
+  {
+    "revision": "6e7074d6dea214b313e412665ccb8a08",
+    "url": "/OspreyAI/static/media/QIMRB.6e7074d6.png"
+  },
+  {
+    "revision": "d0f78ace728f1035a456c9b48ba26d52",
+    "url": "/OspreyAI/static/media/QIMRB_dark.d0f78ace.png"
+  },
+  {
+    "revision": "23a4593e027e96fd2897db7f7b9afd9d",
+    "url": "/OspreyAI/static/media/UQ.23a4593e.png"
+  },
+  {
+    "revision": "917b45061f854131db597f64e1827cf8",
+    "url": "/OspreyAI/static/media/UQ.917b4506.png"
+  },
+  {
+    "revision": "3ad67694f028c6c266d68be9975bc2f7",
+    "url": "/OspreyAI/static/media/bruker_dark.3ad67694.png"
+  },
+  {
+    "revision": "62f568fd80f83f3a0b279e66ba2b3dd9",
+    "url": "/OspreyAI/static/media/bruker_light.62f568fd.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/OspreyAI/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "66fb6330a89ed08074e0e668afdcfe72",
+    "url": "/OspreyAI/static/media/deeplearning.66fb6330.webp"
+  },
+  {
+    "revision": "741c40ac31f2bbc61d2010b9d9cdcfaa",
+    "url": "/OspreyAI/static/media/he.741c40ac.png"
+  },
+  {
+    "revision": "d15597e76c88e6bb096811786b85d275",
+    "url": "/OspreyAI/static/media/hnc.d15597e7.webp"
+  },
+  {
+    "revision": "8e81eb6c8318204abad83a1650ed976e",
+    "url": "/OspreyAI/static/media/immuno.8e81eb6c.png"
+  },
+  {
+    "revision": "704fc8c6acf3afc573bb74b8e3141c75",
+    "url": "/OspreyAI/static/media/multiomics.704fc8c6.png"
+  },
+  {
+    "revision": "205de8c0848275b5fe0507c45a1d6700",
+    "url": "/OspreyAI/static/media/nanostring.205de8c0.png"
+  },
+  {
+    "revision": "cfde977bf40b93ed3bffff10da51548e",
+    "url": "/OspreyAI/static/media/other.cfde977b.png"
+  },
+  {
+    "revision": "580c4f57745d7dc676cc4de36c7c060d",
+    "url": "/OspreyAI/static/media/oxford_dark.580c4f57.png"
+  },
+  {
+    "revision": "76c8fd905ef552007e913ec1b9beba3c",
+    "url": "/OspreyAI/static/media/oxford_light.76c8fd90.png"
+  },
+  {
+    "revision": "dfe899315a949177af51e778c76f084b",
+    "url": "/OspreyAI/static/media/pipeline_dark.dfe89931.png"
+  },
+  {
+    "revision": "9b20cdc06418d3871aa63aefe8b79d93",
+    "url": "/OspreyAI/static/media/pipeline_light.9b20cdc0.png"
+  },
+  {
+    "revision": "cb2489c9a05abf6def063cff22b6f372",
+    "url": "/OspreyAI/static/media/resume.cb2489c9.pdf"
+  },
+  {
+    "revision": "453970cff41f70826ea78661f44dc3b6",
+    "url": "/OspreyAI/static/media/scRNA.453970cf.png"
+  },
+  {
+    "revision": "3c26d9be14254114a41766ca8b6de127",
+    "url": "/OspreyAI/static/media/scST.3c26d9be.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/OspreyAI/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "e0c7400d5c54e79ab201306de43cff88",
+    "url": "/OspreyAI/static/media/sm.e0c7400d.png"
+  },
+  {
+    "revision": "4207e912608bffb44c5f8e9bd0f6eaee",
+    "url": "/OspreyAI/static/media/snp.4207e912.png"
+  },
+  {
+    "revision": "6be8c59e34812247a8f61bbf41a3e23d",
+    "url": "/OspreyAI/static/media/snp.6be8c59e.png"
+  },
+  {
+    "revision": "7c85690182a8017ddc24e73f1036aa69",
+    "url": "/OspreyAI/static/media/spamtp.7c856901.png"
+  },
+  {
+    "revision": "9b89efd7fedb778dc24c651f2b31b642",
+    "url": "/OspreyAI/static/media/spotST.9b89efd7.png"
+  },
+  {
+    "revision": "98573f8c7a3f149e848def3c325315b8",
+    "url": "/OspreyAI/static/media/stomics.98573f8c.png"
+  }
+]);
