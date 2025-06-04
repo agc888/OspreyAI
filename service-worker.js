6ee3e6bcb43767205bf6d78eea2cf44d8d891481
agc888 <agcauser1997@gmail.com>
2025-06-04 21:50:31 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/OspreyAI/precache-manifest.c836908ffa44c2bcaeb8fcee6c367e0b.js"
+  "/OspreyAI/precache-manifest.324216bfa6af0fc1fd8376c2693be86f.js"
 );
 
 self.addEventListener('message', (event) => {
